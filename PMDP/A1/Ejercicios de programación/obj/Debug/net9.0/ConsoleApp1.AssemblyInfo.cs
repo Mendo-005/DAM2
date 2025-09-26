96410898398af4ad3c676099b520b8e0a25c597c
Mendo-005 <mendozachaparro73@gmail.com>
2025-09-26 09:32:16 +0200

    Update

--- PMDP/A1/Ejercicios de programación/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ PMDP/A1/Ejercicios de programación/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0059fd16c2c295f730b08e840c1d51515f3ddd35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbfb2fc00462167744fb2f36d132acd4f31933b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
