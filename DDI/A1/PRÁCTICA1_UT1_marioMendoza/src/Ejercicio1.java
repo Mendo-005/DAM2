cf8bee52315d97466af2683297d4b1ce8d41566d
Mendo-05 <mendozachaparro73@gmail.com>
2025-09-28 13:36:48 +0200

    DDI Portatil Update

--- DDI/A1/PRÁCTICA1_UT1_marioMendoza/src/Ejercicio1.java
+++ DDI/A1/PRÁCTICA1_UT1_marioMendoza/src/Ejercicio1.java
@@ -4,6 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+import VentanaConfirmacion;
+import VentanaMetodosPago;
+
 public class Ejercicio1 {
     
     public static void main(String[] args) {
