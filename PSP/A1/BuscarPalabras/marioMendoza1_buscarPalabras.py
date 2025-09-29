caa39d6b0dca58253a8f27bdb1fe193f3b28238c
Mendo-005 <mendozachaparro73@gmail.com>
2025-09-29 13:21:58 +0200

    PSP Update

--- PSP/A1/BuscarPalabras/marioMendoza1_buscarPalabras.py
+++ PSP/A1/BuscarPalabras/marioMendoza1_buscarPalabras.py
@@ -1,10 +1,19 @@
+import unicodedata
+
+def quitar_acentos(texto):
+    # Normaliza el texto y elimina los acentos
+    texto_normalizado = unicodedata.normalize('NFD', texto)
+    texto_sin_acentos = ''.join(c for c in texto_normalizado if unicodedata.category(c) != 'Mn')
+    return texto_sin_acentos
+
 def contar_palabras():
     try:
             with open('texto.txt', 'r', encoding='utf-8') as archivo:
                 contenido = archivo.read()
 
-            # Convertir a minúsculas 
+            # Convertir a minúsculas y quitar acentos
             contenido = contenido.lower()
+            contenido = quitar_acentos(contenido)
 
             # Dividir el texto en palabras 
             words = contenido.split()
@@ -19,9 +28,10 @@ def contar_palabras():
                     contador_palabras[palabra] = 1
             print(contador_palabras)
             while True:
-                palabraBuscada = input("Introduzca una palabra (o escriba 'salir' para terminar): ").lower()
-                if palabraBuscada == -1:
+                palabraBuscada = input("Introduzca una palabra (o escriba '-1' para terminar): ").lower()
+                if palabraBuscada == '-1':
                     break
+                palabraBuscada = quitar_acentos(palabraBuscada)
                 if palabraBuscada in contador_palabras:
                     print(f"La palabra '{palabraBuscada}' aparece {contador_palabras[palabraBuscada]} veces en el texto")
                 else:
