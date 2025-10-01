09c2d74c48f4975b2f19e0b43f5f85b7c65f2eb9
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-01 12:23:58 +0200

    PSP Update

--- PSP/A1/Procesos/03 MultiProcesos.py
+++ PSP/A1/Procesos/03 MultiProcesos.py
@@ -1,26 +1,33 @@
-import time 
+import time
 import multiprocessing as mp
-import os 
+import os
 
 def hijo(num):
-
-    global valor 
-    print(f"Hijo antes de modificar {valor}")
+    c1 = mp.get_context()
+    global valor
+    print("Hijo antes de modificar: ", valor)
     valor = num
-    print(f"Hijo {valor}")
+    print("Hijo: ", valor)
     time.sleep(num)
-    print("Salgo del proceso hijo{}".format(os.getid()))
-
+    print("Salgo del proceso hijo {}".format(os.getpid()))
+    os._exit(0)
 valor = 0
+
 def main():
     global valor
-    valor = 5
-    # Args es una tupla, al recibir solo un parametro, el fotmato es bi dimensional sin rellenar (3, "vacio")
-    pl = mp.Process(target= hijo, args = (3,))
-    pl.start()
-    print("Proceso creado {}".format(pl.pid))
-    # pl.join() - Sirve para matar un proceso Hijo y seguir con el proceso Padre
-    print(f"Padre {valor}")
+    valor = -5
+    p1 = mp.Process(target=hijo, args=(3,))
+    p1.start()
+    print ("Proceso creado {}".format(p1.pid))
+    #p1.join()
+    print("Padre: ", valor)
+    p2 = mp.Process(target=hijo, args=(1,))
+    p2.start()
+    print ("Proceso creado {}".format(p2.pid))
+    #p2.join()
+    print("Padre: ", valor)
+    print("Salgo del proceso padre {}".format(os.getpid()))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
     main()
