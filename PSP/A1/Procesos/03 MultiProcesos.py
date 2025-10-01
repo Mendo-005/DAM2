74b4b03394feb840e42ea196a21c23dae6c728af
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-01 10:52:13 +0200

    PSP Update

---
+++ PSP/A1/Procesos/03 MultiProcesos.py
@@ -0,0 +1,26 @@
+import time 
+import multiprocessing as mp
+import os 
+
+def hijo(num):
+
+    global valor 
+    print(f"Hijo antes de modificar {valor}")
+    valor = num
+    print(f"Hijo {valor}")
+    time.sleep(num)
+    print("Sakgo dek proceso hijo{}".format(os.getid()))
+
+valor = 0
+def main():
+    global valor
+    valor = 5
+    # Args es una tupla, al recibir solo un parametro, el fotmato es bi dimensional sin rellenar (3, "vacio")
+    pl = mp.Process(target= hijo, args = (3,))
+    pl.start()
+    print("Proceso creado {}".format(pl.pid))
+    # pl.join() - Sirve para matar un proceso Hijo y seguir con el proceso Padre
+    print(f"Padre {valor}")
+
+if __name__ == "__main__":
+    main()
