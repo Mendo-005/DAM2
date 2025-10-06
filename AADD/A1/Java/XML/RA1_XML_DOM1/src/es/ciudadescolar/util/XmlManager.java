92e1b6304be43b40e8f02700adee658e918c0d17
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-06 12:07:38 +0200

    AADD Update

---
+++ AADD/A1/Java/XML/RA1_XML_DOM1/src/es/ciudadescolar/util/XmlManager.java
@@ -0,0 +1,83 @@
+package es.ciudadescolar.util;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlManager 
+{
+    /**
+     * @param ficheroXml
+     * @return
+     */
+    public static List<Alumno> procesarXmlAlumnos(File ficheroXml)
+    {
+        List<Alumno> alumnos = new ArrayList<>();
+
+        Alumno alumno = null;
+
+        DocumentBuilderFactory obt = DocumentBuilderFactory.newInstance();
+
+        obt.setValidating(true); // Cuando el XML se valide se fija el parametro TRUE
+        obt.setIgnoringElementContentWhitespace(true); // Ignorar espacios sin informacion
+        
+        try 
+        {
+            DocumentBuilder db = obt.newDocumentBuilder();
+            
+            db.setErrorHandler(new AlumnoErrorHandler()); // Para personalizar el control de error
+
+            Document documento = db.parse(ficheroXml);
+            
+            Element elementoRaiz = documento.getDocumentElement();
+            //System.out.println("Se didsponen de ["+ elementoRaiz.getChildNodes().getLength()+"]");
+            
+            NodeList listaNodosAlumno = elementoRaiz.getChildNodes();
+
+            for (int i = 0; i < listaNodosAlumno.getLength(); i++) 
+            {
+                Node nodoAlumno = listaNodosAlumno.item(i);
+                // Sabemos que los noods alumnos son elementos
+                if (nodoAlumno.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element elementoAlumno = (Element) nodoAlumno;
+
+                    Node nodoExpediente = elementoAlumno.getFirstChild();
+                    Node nodoNombre = nodoExpediente.getNextSibling();
+                    //Node nodoEdad = nodoAlumno.getLastChild();
+                    Node nodoEdad = nodoNombre.getNextSibling();
+
+                    // Aquí deberías extraer los datos de los nodos y crear el objeto Alumno
+                    // Por ejemplo:
+                    alumno = new Alumno(
+                        nodoExpediente.getTextContent().trim(),
+                        nodoNombre.getTextContent().trim(),
+                        Integer.parseInt(nodoEdad.getTextContent().trim())
+                    );
+                    alumnos.add(alumno);
+                    /*String expediente = nodoExpediente.getTextContent().trim();
+                    String nombre = nodoNombre.getTextContent().trim();
+                    int edad = Integer.parseInt(nodoEdad.getTextContent().trim());
+                    alumno = new Alumno(expediente, nombre, edad);
+                    alumnos.add(alumno);
+                    */
+                }
+                
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException e) 
+        {
+            System.err.println("Error durante el parseo de XML: "+ e.getMessage());
+        }
+        
+        return alumnos;
+    }    
+}
