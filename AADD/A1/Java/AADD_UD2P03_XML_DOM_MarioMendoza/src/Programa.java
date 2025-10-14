3e1dc2aae30f643ca9e4318c49b1bed146b0ada2
Mendo-05 <mendozachaparro73@gmail.com>
2025-10-14 09:39:55 +0200

    AADD Portatil Update

---
+++ AADD/A1/Java/AADD_UD2P03_XML_DOM_MarioMendoza/src/Programa.java
@@ -0,0 +1,13 @@
+import es.ciudadescolar.clases.Noticia;
+import es.ciudadescolar.utils.XMLManager;
+import es.ciudadescolar.utils.TextManager;
+
+
+public class Programa {
+    public static void main(String[] args) throws Exception 
+    {
+        Boolean XMLNoticias = XMLManager.downloadFileFromInternet("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada","XMLNoticias");
+        
+
+    }
+}
