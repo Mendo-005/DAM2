d5ef4b1ca411a4ac2391d93db540d56a6707ef46
Mendo-05 <mendozachaparro73@gmail.com>
2025-10-14 21:03:57 +0200

    DDI Portatil Update

--- AADD/A1/Java/AADD_UD2P03_XML_DOM_MarioMendoza/src/Programa.java
+++ AADD/A1/Java/AADD_UD2P03_XML_DOM_MarioMendoza/src/Programa.java
@@ -7,6 +7,7 @@ public class Programa {
     public static void main(String[] args) throws Exception 
     {
         Boolean XMLNoticias = XMLManager.downloadFileFromInternet("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada","XMLNoticias");
+        System.out.println("XMLNoticias downloaded: " + XMLNoticias);
         
 
     }
