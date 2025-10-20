d93df5732501d4125a2bee7e37b2a05dcbc6b57f
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-20 12:26:18 +0200

    AADD Update

--- AADD/A1/Java/sva-t05-maven/src/main/java/es/ciudadescolar/Main.java
+++ AADD/A1/Java/sva-t05-maven/src/main/java/es/ciudadescolar/Main.java
@@ -1,7 +1,13 @@
 package es.ciudadescolar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        LOG.info("Aplicación iniciada");
     }
 }
