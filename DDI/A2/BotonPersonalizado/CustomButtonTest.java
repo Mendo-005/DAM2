e27458868a248281ef2ec5a5e72f5028640c67f5
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-20 09:48:36 +0200

    DDI Update

---
+++ DDI/A2/BotonPersonalizado/CustomButtonTest.java
@@ -0,0 +1,19 @@
+package PrimeraVentana;
+
+import javax.swing.*;
+
+public class CustomButtonTest {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Prueba de Botón Redondo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+
+        // Crear una instancia del botón redondo personalizado
+        RoundButton roundButton = new RoundButton("Presiona");
+
+        // Añadir el botón a la ventana
+        frame.add(roundButton);
+        frame.setVisible(true);
+    }
+}
+
