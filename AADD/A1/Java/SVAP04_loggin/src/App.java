b8ee5026ebc787899fb269df8a257e8a9ec48948
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-20 11:46:27 +0200

    AADD Update

--- AADD/A1/Java/SVAP04_loggin/src/App.java
+++ AADD/A1/Java/SVAP04_loggin/src/App.java
@@ -7,5 +7,10 @@ public class App {
     public static void main(String[] args) throws Exception 
     {
         LOG.info("Este evento representa una información general de nuestra aplicación.");
+        LOG.warn("Este evento representa una advertencia sobre un posible problema futuro.");
+        LOG.error("Este evento representa un error que ha ocurrido en la aplicación.");
+        LOG.trace("Este evento representa información de seguimiento detallada.");
+        LOG.debug("Este evento representa información de depuración.");
     }
 }
+    
