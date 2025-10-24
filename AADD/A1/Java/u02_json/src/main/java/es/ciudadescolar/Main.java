2741ba7ece828c5b5e1b4163c42fc8ad6897156c
Mendo-005 <mendozachaparro73@gmail.com>
2025-10-24 14:00:20 +0200

    AADD Update

--- AADD/A1/Java/u02_json/src/main/java/es/ciudadescolar/Main.java
+++ AADD/A1/Java/u02_json/src/main/java/es/ciudadescolar/Main.java
@@ -1,10 +1,12 @@
 package es.ciudadescolar;
 
 import java.io.File;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import es.ciudadescolar.instituto.Alumno;
 import es.ciudadescolar.util.JsonManager;
 
 public class Main {
@@ -13,7 +15,20 @@ public class Main {
 
     public static void main(String[] args) 
     {
-
-        JsonManager.parsearJsonAlumnos(new File("alumnos.json"));
+        File ficheroSalidaAlumnod = new File(("nuevosAlumnos.json"));
+        File ficheroJsonAlumnos = new File("alumnos.json");
+        
+        List<Alumno> alumnosJson = JsonManager.parsearJsonAlumnos(ficheroJsonAlumnos);
+        
+        if (alumnosJson.isEmpty())
+        {
+            // Nuevo Json
+            JsonManager.createJsonAlumnos((alumnosJson), ficheroSalidaAlumnod);
+            
+        }
+        else
+        {
+            LOG.warn("No se han recuperado ficheros json: " + ficheroJsonAlumnos.getName());
+        }
     }
 }
--- AADD/A1/Java/u02_json/src/main/java/es/ciudadescolar/util/JsonManager.java
+++ AADD/A1/Java/u02_json/src/main/java/es/ciudadescolar/util/JsonManager.java
@@ -2,14 +2,19 @@ package es.ciudadescolar.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import es.ciudadescolar.instituto.Alumno;
 
@@ -33,13 +38,30 @@ public class JsonManager {
         }
 
         JsonNode nodoRaiz = null;
-
+        JsonNode nodoAlumnos = null; 
         try {
             nodoRaiz = mapper.readTree(ficheroJson);
 
             if (nodoRaiz.isObject())
             {
                 LOG.info("centro: " + nodoRaiz.get("centro").asText());               
+                LOG.info("codigo" + nodoRaiz.get("codigo").asInt());
+                LOG.info("curso" + nodoRaiz.get("curso").asText());
+                
+                nodoAlumnos = nodoRaiz.get("alumnos");
+                if (nodoAlumnos.isArray())
+                {
+                    Alumno alumno = null;
+                    for(JsonNode al: nodoAlumnos)
+                    {
+                        alumno = new Alumno();
+                        alumno.setExpediente(al.get("expediente").asText());
+                        alumno.setEdad(Integer.valueOf(al.get("edad").asInt()));
+                        alumno.setNombre(al.get("nombre").asText());
+                    
+                        listaAlumnos.add(alumno);
+                    }
+                }
             }
 
         } catch (IOException e) 
@@ -47,9 +69,55 @@ public class JsonManager {
             LOG.error(e.getMessage());
         }
         
-        
-        
         return listaAlumnos;
+
+    }
+
+    public static void createJsonAlumnos(List<Alumno> alumnos, File ficheroSalidaJson)
+    {/*
+        // Opcion 1: Creacion dinamica del fichero
+        ArrayNode arrayAlumnos = mapper.createArrayNode();
+        
+        ObjectNode nodoAlumno = null;
+        
+        if((alumnos != null) && (!alumnos.isEmpty()))
+        {
+            for(Alumno al : alumnos)
+            {
+                nodoAlumno = mapper.createObjectNode();
+
+                nodoAlumno.put("nombre",al.getNombre());
+                nodoAlumno.put("expediente",al.getExpediente());
+                nodoAlumno.put("edad",al.getEdad().intValue()); // int porque es Integer
+
+                arrayAlumnos.add(nodoAlumno);
+            }
+
+            try {
+                mapper.writerWithDefaultPrettyPrinter().writeValue(ficheroSalidaJson, arrayAlumnos);
+            } catch (StreamWriteException e) {
+                LOG.error("Error durante la escritura "+ e.getMessage());
+            } catch (DatabindException e) {
+                LOG.error("Error durante la escritura "+ e.getMessage());
+            } catch (IOException e) {
+                LOG.error("Error durante la escritura "+ e.getMessage());
+            }
+        }*/
+
+
+        // Opcion 2: Serializacioón Jackson
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(ficheroSalidaJson, alumnos);
+        } catch (StreamWriteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 }
