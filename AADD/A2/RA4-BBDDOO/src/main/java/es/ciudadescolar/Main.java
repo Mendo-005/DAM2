235ae4626c50540067b25774614fbc2c53cc80a4
Mendo-005 <mendozachaparro73@gmail.com>
2025-11-14 14:10:11 +0100

    update

--- AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/Main.java
+++ AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/Main.java
@@ -1,13 +1,28 @@
 package es.ciudadescolar;
 
 import java.io.File;
+import java.util.List;
 
+import es.ciudadescolar.instituto.Alumno;
 import es.ciudadescolar.utils.Bbddoo;
 
 public class Main {
     public static void main(String[] args) 
     {
         Bbddoo bd = new Bbddoo(new File("alumnos.db4o"), true);
+        
+
+        Alumno alumno = new Alumno("Paco","999",Integer.valueOf(23));
+        Alumno alumno2 = new Alumno("Fermin","888",Integer.valueOf(20));
+        Alumno alumno3 = new Alumno("David","777",Integer.valueOf(19));
+
+        // Guardamos los alumnos
+        bd.guardarAlumno(alumno);
+        bd.guardarAlumno(alumno2);
+        bd.guardarAlumno(alumno3);
+
+        List<Alumno> alumnoEnBD = bd.recuperaTodosAlumnos();
+        // Cerramos base de datos
         bd.cerrar();
     }
 }
--- AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/utils/Bbddoo.java
+++ AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/utils/Bbddoo.java
@@ -1,17 +1,22 @@
 package es.ciudadescolar.utils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+import es.ciudadescolar.instituto.Alumno;
 
 public class Bbddoo 
 {
     private static final Logger LOG = LoggerFactory.getLogger(Bbddoo.class);
-    private ObjectContainer bd = null;
+    private static ObjectContainer bd = null;
     private File ficheroBd = null;
 
     public Bbddoo(File fich, boolean sobrescribir)
@@ -44,4 +49,60 @@ public class Bbddoo
         return false;
     }
 
+    public static boolean guardarAlumno(Alumno alumno) 
+    {
+        boolean status = false;
+
+        if(bd != null)
+        {
+            bd.store(alumno);
+            status = true;
+            LOG.info("Alumno guardado: " + alumno);
+        }
+        return status;
+    }
+
+    public List<Alumno> recuperaTodosAlumnos()
+    {
+        List<Alumno> alumnos = null;
+        // Preparamos una plantilla inclusivas (que todos los alumnos dela BD satisfagan)
+        // String = null
+        // Int = 0
+        Alumno alumnoBuscado = new Alumno(null, null, 0);
+        
+        ObjectSet<Alumno> alumnosRecuperados = bd.queryByExample(alumnoBuscado);
+
+        if (!alumnosRecuperados.isEmpty()) 
+        {
+            alumnos = new  ArrayList<>();
+            for (Alumno alumno : alumnosRecuperados) 
+            {
+                LOG.info("Alumno recuprado: "+ alumno);
+                alumnos.add(alumno);    
+            }
+        }
+        return alumnos;
+    }
+
+    public Alumno recuperaAlumnoPorExp(String expediente)
+    {
+        // Preparamos una plantilla inclusivas (que todos los alumnos dela BD satisfagan)
+        // String = null
+        // Int = 0
+        Alumno alumnoBuscado = new Alumno(null, expediente, 0);
+        Alumno alumnoRecuprado = null;
+        ObjectSet<Alumno> alumnosRecuperados = null;
+
+        if (!alumnosRecuperados.isEmpty()) 
+        {
+            alumnosRecuperados = bd.queryByExample(alumnoBuscado);
+            alumnos = new  ArrayList<>();
+            for (Alumno alumno : alumnosRecuperados) 
+            {
+                LOG.info("Alumno recuprado: "+ alumno);
+                alumnos.add(alumno);    
+            }
+        }
+        return alumnos;
+    }
 }
