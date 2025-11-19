debd675773317ce28d8e5510f21a85913134d412
Mendo-005 <mendozachaparro73@gmail.com>
2025-11-19 09:39:44 +0100

    update

--- AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/Main.java
+++ AADD/A2/RA4-BBDDOO/src/main/java/es/ciudadescolar/Main.java
@@ -3,18 +3,25 @@ package es.ciudadescolar;
 import java.io.File;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import es.ciudadescolar.instituto.Alumno;
 import es.ciudadescolar.utils.Bbddoo;
 
 public class Main {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    @SuppressWarnings("static-access")
     public static void main(String[] args) 
     {
         Bbddoo bd = new Bbddoo(new File("alumnos.db4o"), true);
         
 
-        Alumno alumno = new Alumno("Paco","999",Integer.valueOf(23));
-        Alumno alumno2 = new Alumno("Fermin","888",Integer.valueOf(20));
-        Alumno alumno3 = new Alumno("David","777",Integer.valueOf(19));
+        Alumno alumno = new Alumno("Paco","999", 23);
+        Alumno alumno2 = new Alumno("Fermin","888", 20);
+        Alumno alumno3 = new Alumno("David","777", 19);
 
         // Guardamos los alumnos
         bd.guardarAlumno(alumno);
@@ -22,6 +29,21 @@ public class Main {
         bd.guardarAlumno(alumno3);
 
         List<Alumno> alumnoEnBD = bd.recuperaTodosAlumnos();
+        LOG.info("Alumnos: " + alumnoEnBD);
+        bd.borrarAlumno(alumno);
+        bd.recuperaTodosAlumnos();
+        
+        // Borrar todos los alumos
+        bd.borrarTodosAlumnos();
+        LOG.info("Borrados todos los alumnos");
+
+        // Modificar alumnosa
+        Alumno alumno4 = new Alumno("Dario","3435", 23);
+        Alumno alumno5 = new Alumno("Maria","2212", 23);
+        bd.guardarAlumno(alumno4);
+        bd.guardarAlumno(alumno5);
+
+        bd.modificarAlumno(alumno5, 20);
         // Cerramos base de datos
         bd.cerrar();
     }
