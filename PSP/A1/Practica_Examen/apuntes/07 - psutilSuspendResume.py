9ba9bb7649b49d97935fb9be327cb06ba4cbc36c
Mendo-005 <mendozachaparro73@gmail.com>
2025-11-25 08:21:17 +0100

    update

--- PSP/A1/Practica_Examen/apuntes/07 - psutilSuspendResume.py
+++ PSP/A1/Practica_Examen/apuntes/07 - psutilSuspendResume.py
@@ -1,9 +1,12 @@
+import os
 import psutil
 import time
 
 time.sleep(1)
+os.open("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/Accessories", 0)
+
 for p in psutil.process_iter():
-	#print(p.name())
+	print(p.name())
 	if p.is_running():
 		if p.name() == "Notepad.exe":
 			print("Encontrado Notepad")
