46941d85e9d4a5bce77798d93a8fc62a7f33625f
Mendo-005 <mendozachaparro73@gmail.com>
2026-02-11 10:18:10 +0100

    update

--- AADD/A5/SVA_SPRING_BOOT_CONSOLA_NAVIDAD25-main/SVA_SPRING_BOOT_CONSOLA_NAVIDAD25-main/src/main/java/es/ciudadescolar/navidad25/Navidad25Application.java
+++ AADD/A5/SVA_SPRING_BOOT_CONSOLA_NAVIDAD25-main/SVA_SPRING_BOOT_CONSOLA_NAVIDAD25-main/src/main/java/es/ciudadescolar/navidad25/Navidad25Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import es.ciudadescolar.navidad25.model.Nino;
+import es.ciudadescolar.navidad25.model.Nino.Comportamiento;
 import es.ciudadescolar.navidad25.service.NinoService;
 
 @SpringBootApplication
@@ -14,24 +15,21 @@ public class Navidad25Application implements CommandLineRunner{
 
 	private static final Logger LOG = LoggerFactory.getLogger(Navidad25Application.class);
 
-	private final NinoService ninoService;
-	public Navidad25Application(NinoService ninoService1)
+	private final NinoService ninoServicio;
+	public Navidad25Application(NinoService ninoServ)
 	{
-		this.ninoService = ninoService1;
+		this.ninoServicio = ninoServ;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Navidad25Application.class, args);
 	}
-	
 	@Override
 	public void run(String... args) throws Exception {
 			// lo que antes hacíamos en el main, ahora lo hacemos aquí
 			LOG.info("Hola Spring Boot");
-
-			Nino nino1 = ninoService.registrarNino("Juancho", Nino.Comportamiento.bueno);
-			LOG.info(nino1.toString());
-			
+			Nino ninoJuancho =  ninoServicio.registrarNino("Juancho", Comportamiento.bueno);
+			LOG.info(ninoJuancho.toString());
 	}
 
 }
