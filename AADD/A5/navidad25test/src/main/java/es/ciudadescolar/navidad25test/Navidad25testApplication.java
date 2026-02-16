8d5e678c633a326da11a5b7353fe7a9d50888bd8
Mendo-005 <mendozachaparro73@gmail.com>
2026-02-16 11:58:27 +0100

    update

---
+++ AADD/A5/navidad25test/src/main/java/es/ciudadescolar/navidad25test/Navidad25testApplication.java
@@ -0,0 +1,13 @@
+package es.ciudadescolar.navidad25test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Navidad25testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Navidad25testApplication.class, args);
+	}
+
+}
---
+++ AADD/A5/navidad25test/src/main/java/es/ciudadescolar/navidad25test/controller/NinoController.java
@@ -0,0 +1,11 @@
+package es.ciudadescolar.navidad25test.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/ninos")
+public class NinoController {
+
+
+
+}
