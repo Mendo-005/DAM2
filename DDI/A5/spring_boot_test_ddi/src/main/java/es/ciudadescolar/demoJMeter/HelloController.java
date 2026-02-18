520b1763fefe184d3113b3f252d4d31ce18e3232
Mendo-005 <mendozachaparro73@gmail.com>
2026-02-18 08:29:18 +0100

    update

---
+++ DDI/A5/spring_boot_test_ddi/src/main/java/es/ciudadescolar/demoJMeter/HelloController.java
@@ -0,0 +1,13 @@
+package es.ciudadescolar.modelos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "¡Hola DAM2 desde Spring Boot!";
+    }
+}
